15f3b03402fccd98b86dfef9654308a1d2365b81
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2025-05-07 12:30:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 USERNAME = environ.get('USERNAME', 'https://telegram.me/Sandymaiwait')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002433610423'))
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002694840394').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Sandy2:Sandy2@cluster0.71n6ahq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Sandy3:Sandy3@cluster0.493suxy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://Sandy2:Sandy2@cluster0.71n6ahq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rahul")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Rahul')
